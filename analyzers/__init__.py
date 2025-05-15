45445dbd144bcb65f8814ca18113791a2694b709
admin <fmlbeastmode@gmail.com>
2025-05-15 07:42:17 +0200

    vision agent, web agent

--- analyzers/__init__.py
+++ analyzers/__init__.py
@@ -8,5 +8,7 @@ from analyzers.binary_analyzer import analyze_binary
 from analyzers.cipher_analyzer import analyze_ciphers
 from analyzers.encoding_analyzer import analyze_encodings
 from analyzers.image_analyzer import analyze_image
+from analyzers.web_analyzer import analyze_web
+from analyzers.vision_analyzer import analyze_vision
 
 # Import additional analyzers as they are added
